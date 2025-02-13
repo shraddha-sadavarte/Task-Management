49fc2228ec158947bae3cd5377a7ab5c275929df
Sadavarte Shraddha <shraddhasadavarte24@gmail.com>
2025-02-13 13:34:33 +0530

    frontend app created and basic starting of website creation

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Home from './pages/Home'
+
+function App() {
+
+
+  return (
+    <div className='bg-gray-900 text-white h-screen p-2'>
+      <Home />
+    </div>
+  )
+}
+
+export default App
