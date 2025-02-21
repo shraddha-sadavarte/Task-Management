a9280b897e33b9cb7eb4b6ea899b4abe70f2a6fd
Sadavarte Shraddha <shraddhasadavarte24@gmail.com>
2025-02-21 17:03:01 +0530

    Add tasks card added

--- frontend/src/components/home/Cards.jsx
+++ frontend/src/components/home/Cards.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {CiHeart} from 'react-icons/ci'
 import {FaEdit} from 'react-icons/fa'
 import { MdDelete } from 'react-icons/md';
+import {IoAddCircleSharp} from 'react-icons/io5'
 
 const Cards = () => {
 
@@ -24,7 +25,7 @@ const Cards = () => {
         },
     ];
   return (
-    <div className='grid grid-cols-4 gap-4 p-4'>
+    <div className='grid grid-cols-3 gap-4 p-4'>
       {data && data.map((items,i) => (
         <div className=' flex flex-col justify-between bg-gray-700 rounded-sm p-4'>
             <div>
@@ -47,6 +48,11 @@ const Cards = () => {
             </div>
         </div>
       ))}
+
+      <div className='flex flex-col justify-center items-center bg-gray-700 rounded-sm p-4 text-gray-300 hover:cursor-pointer hover:scale-105 transition-all duration-300'>
+        <IoAddCircleSharp className='text-5xl'/>
+        <h2 className='text-2xl mt-4'>Add Task</h2>
+      </div>
     </div>
   )
 }
