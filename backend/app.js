dc8596452a85ddb0cc14b725f570bd29bd5bb326
Sadavarte Shraddha <shraddhasadavarte24@gmail.com>
2025-02-25 14:32:02 +0530

    mongodb connected successfully

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,17 @@
-import express from 'express'
+import express from 'express';
+import connectDB from './conn/conn.js'; 
 
 const app = express();
 
-app.use("/", (req,res) => {
-    res.send("Hello from backend side")
+// Connect to MongoDB
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Hello from backend side");
 });
 
-const port = 1000
+const port = 1000;
 
-app.listen(port, () => {console.log("Server Started")})
-
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
