e358976c2aa4cfd83de35e7bb5f0ecdbe8faa81c
Sadavarte Shraddha <shraddhasadavarte24@gmail.com>
2025-03-21 18:03:01 +0530

    logout functionality

--- frontend/src/components/home/Sidebar.jsx
+++ frontend/src/components/home/Sidebar.jsx
@@ -3,7 +3,9 @@ import {CgNotes} from "react-icons/cg";
 import {MdLabelImportant} from 'react-icons/md';
 import {FaCheckDouble} from 'react-icons/fa6';
 import {TbNotebookOff} from 'react-icons/tb';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { authActions } from '../../store/auth';
 
 const Sidebar = () => {
 
@@ -30,11 +32,19 @@ const Sidebar = () => {
         },
     ];
 
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const logout = () => {
+        dispatch(authActions.logout());
+        localStorage.clear();
+        navigate("/login");
+    }
+
   return (
     <>
     <div>
       <h2 className='text-xl font-semibold'>Shraddha Coding</h2>
-      <h4 className='mb-1 text-grey-200'>shraddha24@gmail.com</h4>
+      <h4 className='mb-1 text-gray-200'>shraddha24@gmail.com</h4>
       <hr />
     </div>
 
@@ -48,7 +58,7 @@ const Sidebar = () => {
     </div>
 
     <div>
-        <button className='bg-gray-600 w-full p-2 rounded cursor-pointer'>Log out</button>
+        <button className='bg-gray-500 hover:bg-red-600 text-white w-full p-2 rounded cursor-pointer mt-4' onClick={logout}>Log out</button>
     </div>
 
     </>
